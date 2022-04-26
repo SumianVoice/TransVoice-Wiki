ec2f5edb8fe1d52c8e06d4eedbebe449f0330595
Sumyjkl <sumianvoice@gmail.com>
2022-04-26 17:05:44 +1000

    added iframe builder to reduce lag

---
+++ js/buildiframe.js
@@ -0,0 +1,25 @@
+
+function buildiframe_yt(id="null",src="https://wiki.sumianvoice.com/404") {
+  let x = document.getElementById(id);
+  if (!x) {return false;}
+  x.outerHTML = `
+  <p align="left">
+      <iframe
+      width="560"
+      height="315"
+      src="${src}"
+      title="YouTube video player"
+      frameborder="0"
+      allow=
+      "
+        accelerometer;
+        autoplay;
+        clipboard-write;
+        encrypted-media;
+        gyroscope;
+        picture-in-picture
+      "
+      allowfullscreen></iframe>
+  </p>`;
+}
+// loading="lazy"
